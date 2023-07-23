4a33774718306ef9a05b3f3ee881de66bffbba7b
Enoch E Jacobs <136347551+ejacobs32@users.noreply.github.com>
2023-07-23 15:07:31 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / '/static/'
-STATIC_ROOT = BASE_DIR / ''
+STATIC_ROOT = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
