947a2d13be53d26dd4aa7db26bbd168fe69b1932
ejacobs32 <ejacobs32@ivytech.edu>
2023-07-23 14:34:50 -0400

    go

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / '/static/'
+#STATIC_ROOT = BASE_DIR / '/static/'
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
